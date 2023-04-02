d7927b3fe292fb38a97f297eca7be0a7ec123154
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-04-01 23:59:05 -0500

    first commit

--- app/components/Item/AddItem.js
+++ app/components/Item/AddItem.js
@@ -5,7 +5,7 @@ import {
   Text,
   View,
   TextInput,
-  TouchableOpacity,
+  TouchableOpacity, Image
 } from "react-native";
 import { Divider } from "react-native-elements";
 import ItemData from "./ItemData";
@@ -36,26 +36,18 @@ export default function AddItem() {
     }
   };
 
-//   To add item with title & description
-  // const [title, setTitle] = useState("");
-  // const [description, setDescription] = useState("");
-
-  // const handelSubmit = () => {
-  //   if (title && description) {
-  //     const item = {
-  //       title,
-  //       description,
-  //     };
-  //     setTitle("");
-  //     setDescription("");
-  //   } else {
-  //     alert("Please enter the title and description.");
-  //   }
-  // };
 
   return (
     <>
       <SafeAreaView style={styles.screen}>
+      <View style={styles.containerTwo}>
+                <Image
+                style={styles.image}
+                source={require("../../../assets/shoe.png")}
+               /> 
+               <Text style={{ marginTop: 20, fontSize: 35, paddingLeft:5, color: "midnightblue"}}>Shopping App</Text>
+
+               </View>
         <View style={styles.viewContainer}>
           <Text style={styles.title}>Add Items</Text>
           <TextInput
@@ -86,38 +78,62 @@ export default function AddItem() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: "#ffe4b5",
+    backgroundColor: "aqua",
     alignItems: "center",
     justifyContent: "center",
+    width: "100%"
   },
+  image: {
+    height: 70,
+    width: 70,
+    marginTop: 5,
+
+    borderRadius: 35,
+    padding: 30,
+},
+containerTwo: {
+
+
+  flex: 1,
+  backgroundColor: "aquamarine",
+  flexDirection: "row",
+  padding: 40,
+  alignItems: "stretch",
+  justifyContent: "flex-start",
+  width: "90%"
+
+
+},
   button: {
-    backgroundColor: "blue",
+    backgroundColor: "yellow",
     borderRadius: 15,
     paddingVertical: 10,
     paddingHorizontal: 20,
     marginTop: 250,
+    width: "25%"
   },
   buttonText: {
     color: "white",
-    fontSize: 18,
+    fontSize: 20,
     textAlign: "center",
   },
   viewContainer: {
     flex: 1,
-    padding: 50,
+    padding: 45,
     backgroundColor: "#ffe4b5",
+    width: "90%"
   },
   title: {
     color: "black",
-    fontSize: 50,
+    fontSize: 45,
     textAlign: "center",
   },
   input: {
-    height: 60,
-    margin: 12,
-    borderWidth: 1,
-    padding: 20,
-    borderColor: "#ffe4b5",
+    height: 58,
+    margin: 10,
+    borderWidth: 2,
+    padding: 25,
+    borderColor: "green",
     borderBottomColor: "black",
   },
 });
